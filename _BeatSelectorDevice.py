157514b14a44b0ffec0e4dcb70b54e6c9b3e19a1
nodemon <bernelectronic@gmail.com>
2023-02-20 16:15:37 -0500

    clip selection v3

--- _BeatSelectorDevice.py
+++ _BeatSelectorDevice.py
@@ -11,20 +11,48 @@ class BeatSelectorDevice(EbiagiComponent):
         self._device = device
         self._instrument = instrument
 
-        self.log('create beat selector')
+        self.beats = self.get_beats()
 
-        self._device.view.add_selected_chain_listener(self.on_selected_chain_change)
+        self.select_knob = self.get_select_knob()   
+        self.select_knob.add_value_listener(self.on_select_knob_change)
 
-    def on_selected_chain_change(self):
+
+    def on_select_knob_change(self):
         if not self._device.is_active:
             return
         self.midi_action(self.activate_devices)
 
     def activate_devices(self):
+        selected_beat = self.get_selected_beat()
         for i, chain in enumerate(self._device.chains):
-            if chain == self._device.view.selected_chain:
+            if chain.name == selected_beat:
                 for device in chain.devices:
                     device.parameters[0].value = device.parameters[0].max
             else:
                 for device in chain.devices:
                     device.parameters[0].value = device.parameters[0].min
+
+    def get_select_knob(self):
+        for param in self._device.parameters:
+            if param.name == 'BEAT SELECT':
+                return param
+
+    def get_beats(self):
+        beats = []
+        for param in self._device.parameters:
+            if param.name != 'Device On' and param.name != 'BEAT SELECT' and param.name != 'CLIP SELECT' and not param.name.startswith('Macro'):
+                beats.append(param)
+        return beats
+
+    def get_selected_beat(self):
+        beats = self.beats       
+        current_beat = beats[0]
+        selected_value = self.select_knob.value
+        for b in self.beats:
+            if b.value <= selected_value and b.value > current_beat.value:
+                current_beat = b
+        return current_beat.name
+
+    def disconnect(self):
+        super(BeatSelectorDevice, self).disconnect()
+        self.select_knob.remove_value_listener(self.on_selected_knob_change)
--- _ClipSelectorDevice.py
+++ _ClipSelectorDevice.py
@@ -3,7 +3,6 @@ import math
 from ._EbiagiComponent import EbiagiComponent
 from ._naming_conventions import *
 import Live
-import threading
 
 class ClipSelectorDevice(EbiagiComponent):
 
@@ -12,19 +11,24 @@ class ClipSelectorDevice(EbiagiComponent):
         self._device = device
         self._instrument = instrument
 
+        self.select_knob = self.get_select_knob()
         self.quantization_knob = self.get_quantization_knob()
-        self._device.view.add_selected_chain_listener(self.on_selected_chain_change)
 
-        self.interval = threading.Timer(5.0, self.refresh_track_selection)
-        self.interval.start()
+        self.select_knob.add_value_listener(self.on_select_knob_change)
+
+        self.variations = self.get_variations()
+
         self.lock = False
 
-    def on_selected_chain_change(self):
+    def on_select_knob_change(self):
+        if not self._device.is_active or self._instrument._track.playing_slot_index < 0:
+            return
         clips = []
+        selected_variation = self.get_selected_variation()
         for track in [self._instrument._track] + self._instrument._ex_tracks:
             for i, clip_slot in enumerate(track.clip_slots):
                 if clip_slot.has_clip:
-                    if parse_clip_name(clip_slot.clip.name) == self._device.view.selected_chain.name:
+                    if parse_clip_name(clip_slot.clip.name) == selected_variation:
                         clips.append(clip_slot.clip)
         if len(clips) > 0:
             quantization = math.floor(self.quantization_knob.value)
@@ -43,26 +47,39 @@ class ClipSelectorDevice(EbiagiComponent):
         for clip in clips:
             l = clip.legato
             clip.legato = 1
-            if self._device.is_active:
+            if self._device.is_active and not clip.is_playing and clip:
                 clip.fire()
             clip.legato = l
             clip.launch_quantization = return_quantization
             self.lock = False
+
+    def get_select_knob(self):
+        for param in self._device.parameters:
+            if param.name == 'CLIP SELECT':
+                return param
 
     def get_quantization_knob(self):
         for param in self._device.parameters:
             if param.name == 'QUANTIZATION':
                 return param
 
-    def refresh_track_selection(self):
-        device = self.canonical_parent.song().view.selected_track
-        self.canonical_parent.song().view.selected_track = self._instrument._track
-        self.canonical_parent.song().view.selected_track = device
-        # self.interval = threading.Timer(5.0, self.refresh_track_selection)
-        # self.interval.start()
+    def get_variations(self):
+        variations = []
+        for param in self._device.parameters:
+            if param.name != 'Device On' and param.name != 'CLIP SELECT' and param.name != 'QUANTIZATION' and not param.name.startswith('Macro'):
+                variations.append(param)
+        return variations
+
+    def get_selected_variation(self):
+        variations = self.variations       
+        current_variation = variations[0]
+        selected_value = self.select_knob.value
+        for v in self.variations:
+            if v.value <= selected_value and v.value > current_variation.value:
+                current_variation = v
+        return current_variation.name
 
     def disconnect(self):
         super(ClipSelectorDevice, self).disconnect()
-        self._device.view.remove_selected_chain_listener(self.on_selected_chain_change)
-        self.interval.cancel()
+        self.select_knob.remove_value_listener(self.on_selected_knob_change)
 
